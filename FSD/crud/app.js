95f7477d0a12ac5eaed1ef01aaf99c09b552d271
SantoshiBorapareddy <93024296+b-santoshi@users.noreply.github.com>
2022-09-08 17:38:05 +0530

    Add files via upload

---
+++ FSD/crud/app.js
@@ -0,0 +1,14 @@
+const express =require('express')
+const mongoose =require('mongoose')
+const url= 'mongodb://localhost/AlienDBex'
+const app= express()
+mongoose.connect(url,{useNewUrlParser:true})
+const con= mongoose.connection
+con.on('open' ,() =>
+{
+    console.log('connected')
+})
+app.listen(9000,() =>
+{
+    console.log('serverstarted')
+})
